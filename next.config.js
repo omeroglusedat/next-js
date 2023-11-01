1308194bf0cdf1c5760475403b768b1ea2e34b58
omeroglusedat <omeroglusedat@hotmail.com>
2023-11-01 12:42:52 +0300

    verc3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 
 const path = require('path')
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/users',
+        destination: '/users',
+        permanent: true,
+      },
+      {
+        source: '/users/:id',
+        destination: '/users/:id',
+        permanent: true,
+      },
+    ];
+  },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
       },
